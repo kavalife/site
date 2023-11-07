de1da95b8fed618961243f2b21ce9f691f8eff0a
kavalife <149783601+kavalife@users.noreply.github.com>
2023-11-07 20:49:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-// Підключаємося до Google Таблиці за допомогою Tabletop.js
 var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1HqlJuNMoytkSh9tJjdSu1_Qg7laXC5gh';
 
 function init() {
     Tabletop.init({
         key: publicSpreadsheetUrl,
         callback: showInfo,
-        simpleSheet: true
+        simpleSheet: true,
+        wanted: ['Worksheet'] // Замініть 'YourSheetName' на назву вашого листка
     });
 }
 
 function showInfo(data, tabletop) {
-    // Отримуємо елемент div для вставки даних
     var productsDiv = document.getElementById('products');
+    var errorDiv = document.getElementById('error');
 
-    // Рендеримо дані у форматі магазину
+    if (data.length === 0) {
+        errorDiv.innerText = 'Помилка: Дані не були знайдені в таблиці.';
+        return;
+    }
+
+    productsDiv.innerHTML = '';
+
     for (var i = 0; i < data.length; i++) {
         var product = data[i];
         var productHtml = `
@@ -28,5 +34,14 @@ function showInfo(data, tabletop) {
     }
 }
 
-// Запускаємо ініціалізацію після завантаження сторінки
+function showError(error) {
+    var errorDiv = document.getElementById('error');
+    errorDiv.innerText = 'Помилка отримання даних з таблиці: ' + error.message;
+}
+
 window.addEventListener('DOMContentLoaded', init);
+
+// Обробка помилок під час завантаження таблиці
+window.addEventListener('TabletopLoadError', function (e) {
+    showError(e.detail);
+});
