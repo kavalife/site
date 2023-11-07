fe21c5bfcfa31019958bed425f997452742e4c74
kavalife <149783601+kavalife@users.noreply.github.com>
2023-11-07 20:30:12 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// Підключаємося до Google Таблиці за допомогою Tabletop.js
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1HqlJuNMoytkSh9tJjdSu1_Qg7laXC5gh/edit?usp=sharing&ouid=101292332725883462634&rtpof=true&sd=true';
+
+function init() {
+    Tabletop.init({
+        key: publicSpreadsheetUrl,
+        callback: showInfo,
+        simpleSheet: true
+    });
+}
+
+function showInfo(data, tabletop) {
+    // Отримуємо елемент div для вставки даних
+    var productsDiv = document.getElementById('products');
+
+    // Рендеримо дані у форматі магазину
+    for (var i = 0; i < data.length; i++) {
+        var product = data[i];
+        var productHtml = `
+            <div class="product">
+                <h2>${product['назва моделі']}</h2>
+                <p>Ціна: ${product['ціна'] * product['курс']} грн</p>
+                <p>Наявність: ${product['наявність'] === '✅' ? 'Є в наявності' : 'Немає в наявності'}</p>
+            </div>
+        `;
+
+        productsDiv.innerHTML += productHtml;
+    }
+}
+
+// Запускаємо ініціалізацію після завантаження сторінки
+window.addEventListener('DOMContentLoaded', init);
